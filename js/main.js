9d9e56abdbd871b2695dd72e4de900d13f780b79
Artem Vodianytskyi <vodianitsky@gmail.com>
2016-04-05 18:39:04 +0300

    trying to make fixed rows

--- js/main.js
+++ js/main.js
@@ -6,31 +6,62 @@ function tableInit(block, width, height, cellWidth, cellHeight){
   var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
   var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 
+
+
   if (width == undefined)  {parentBlock.style.width  = (viewportWidth + "px");}
   if (height == undefined) {parentBlock.style.height = (viewportHeight + "px");}
   if (cellWidth == undefined) {cellWidth = 80;}
   if (cellHeight == undefined) {cellHeight = 20;}
 
-  rowsInitialAmount = Math.ceil(viewportHeight / cellHeight);
-  columnsInitialAmount = Math.ceil(viewportWidth / cellWidth);
+  var rowsInitialAmount = Math.ceil(viewportHeight / cellHeight);
+  var columnsInitialAmount = Math.ceil(viewportWidth / cellWidth);
 
-  var creatingTableDom = (function(){
+  var tableWrap = document.createElement("DIV");
+  tableWrap.id = "super-table-wrap";
+  parentBlock.appendChild(tableWrap);
+
+  var creatingGridTableDom = (function() {
+
     var table = document.createElement("TABLE");
-    table.id = "super-table";
+    table.id = "super-table-grid";
     parentBlock.appendChild(table);
 
-    for (var i = 0; i < rowsInitialAmount; i++) {
+    for (var i = 0; i < rowsInitialAmount + 10; i++) {
       var row = document.createElement("TR");
-      row.id = "row" + i;
+      row.id = "row-grid" + i;
       table.appendChild(row);
-      for (var j = 0; j < columnsInitialAmount; j++) {
+      for (var j = 0; j < columnsInitialAmount + 10; j++) {
         var cell = document.createElement("TD");
-        cell.id = "cell" + charArray[j] + i;
+        cell.id = "cell-grid" + charArray[j] + i;
+        cell.width = "80px";
         row.appendChild(cell);
       }
     }
+    tableWrap.onscroll = function() {
+      console.log(tableWrap.scrollTop)
+      parentBlock.scrollTop = tableWrap.scrollTop;
+    }
   })();
 
+  var creatingTableDom = (function() {
+
+    var table = document.createElement("TABLE");
+    table.id = "super-table";
+    tableWrap.appendChild(table);
+
+    for (var i = 0; i < rowsInitialAmount + 10; i++) {
+      var row = document.createElement("TR");
+      row.id = "row" + i;
+      table.appendChild(row);
+      for (var j = 0; j < columnsInitialAmount + 10; j++) {
+        var cell = document.createElement("TD");
+        cell.id = "cell" + charArray[j] + i;
+        cell.width = "80px";
+        row.appendChild(cell);
+      }
+    }
+
+  })();
 
   console.log(rowsInitialAmount);
   console.log(columnsInitialAmount);
