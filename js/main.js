4bad94beb97d131c1cfaf97c6300efff6b1ea3b1
unknown <Vodianitsky Artem>
2016-04-05 10:01:14 +0300

    start of making markup on JS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
-function getJSONData(path, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
-        }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send(); 
-}
+tableInit("table-wrap");
+function tableInit(block, width, height, cellWidth, cellHeight){
+  var charArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var parentBlock = document.getElementById(block);
+
+  var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+  var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+
+  if (width == undefined)  {parentBlock.style.width  = (viewportWidth + "px");}
+  if (height == undefined) {parentBlock.style.height = (viewportHeight + "px");}
+  if (cellWidth == undefined) {cellWidth = 80;}
+  if (cellHeight == undefined) {cellHeight = 20;}
+
+  rowsInitialAmount = Math.ceil(viewportHeight / cellHeight);
+  columnsInitialAmount = Math.ceil(viewportWidth / cellWidth);
+
+  var creatingTableDom = (function(){
+    var table = document.createElement("TABLE");
+    table.id = "super-table";
+    parentBlock.appendChild(table);
+
+    for (var i = 0; i < rowsInitialAmount; i++) {
+      var row = document.createElement("TR");
+      row.id = "row" + i;
+      table.appendChild(row);
+      for (var j = 0; j < columnsInitialAmount; j++) {
+        var cell = document.createElement("TD");
+        cell.id = "cell" + charArray[j] + i;
+        row.appendChild(cell);
+      }
+    }
+  })();
 
 
-getJSONData('/json.php', function(data){
-    console.log(data);
-    console.log(data.a);
-});
+  console.log(rowsInitialAmount);
+  console.log(columnsInitialAmount);
+};
