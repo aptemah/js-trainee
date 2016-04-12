ae73b4585d1285c7a42fb875c4ee7add09621f80
Artem Vodianytskyi <vodianitsky@gmail.com>
2016-04-12 16:50:46 +0300

    All function were inserted to object sTable

--- js/main.js
+++ js/main.js
@@ -1,293 +1,299 @@
-if (localStorage.getItem('sheets')) {
-  var sheetObject = JSON.parse(localStorage.sheets);
-} else {
-  var sheetObject = {};
-};
+
+var sTable = {};
+
+  if (localStorage.getItem('sheets')) {
+    sTable.sheetObject = JSON.parse(localStorage.sheets);
+  } else {
+    sTable.sheetObject = {};
+  };
+
+  sTable.init = function (block, width, height, cellWidth, cellHeight) {
+
+    sTable.tabGenerating()
+
+    var charArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var parentBlock = document.getElementById(block);
+
+    var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    var heightOfTable = viewportHeight - parseInt(window.getComputedStyle(parentBlock).getPropertyValue('margin-top'));
+    if (width == undefined)  {parentBlock.style.width  = (viewportWidth + "px");}
+    if (height == undefined) {parentBlock.style.height = (heightOfTable + "px");}
+    if (cellWidth == undefined) {cellWidth = 80;}
+    if (cellHeight == undefined) {cellHeight = 20;}
+
+    var rowsInitialAmount = Math.ceil(viewportHeight / cellHeight);
+    var columnsInitialAmount = Math.ceil(viewportWidth / cellWidth);
+
+    //создание DOM таблицы
+    var creatingTableDom = (function() {
+
+      var tableFragment = document.createDocumentFragment();
+      var table = document.createElement("TABLE");
+      table.id = "super-table";
+      tableFragment.appendChild(table)
+
+      for (var i = 0; i < rowsInitialAmount; i++) {
+        var row = table.insertRow();
+        for (var j = 0; j < columnsInitialAmount; j++) {
+          row.insertCell();
+        }
+      }
+      parentBlock.appendChild(table);
+      sTable.fillCells();//Заполняем ячейки
+      //Навешиваем события на клик и блур
+      table.addEventListener("click", function(e){
+        if (e.target.tagName == "TD") {
+
+          var rowIndex = e.target.parentElement.rowIndex;
+          var cellIndex = e.target.cellIndex;
+          var targetCell = table.tBodies[0].rows[rowIndex].cells[cellIndex];
+          var input = document.createElement("INPUT");
+
+          input.value = targetCell.textContent;
+          targetCell.textContent = null;
+          targetCell.appendChild(input);
+          input.focus();
+
+          input.onblur = function(){
+
+            var currentSheet = document.querySelector("[name='tab']:checked").value;
+
+            if (!sTable.sheetObject[currentSheet].hasOwnProperty(rowIndex)) {
+              sTable.sheetObject[currentSheet][rowIndex] = {};
+            }
+
+            sTable.sheetObject[currentSheet][rowIndex][cellIndex] = input.value;
+
+            sTable.updateSheet();
+
+            input.parentElement.textContent = input.value;
+            input.remove();
+            input = null;
+
+          };
+        }
+      });
+
+    })();
+
+  };
 
 
-(function tabGenerating(){
+  sTable.tabGenerating = function(){
 
-  var tabsWrap = document.querySelector("#tabs-wrap");
-  var button = document.createElement("BUTTON");
-  button.textContent = "+";
-  button.addEventListener("click", function(e){
-    tabCreate(undefined, undefined, undefined, true);
-  });
-  tabsWrap.appendChild(button);
+    var tabsWrap = document.querySelector("#tabs-wrap");
+    var button = document.createElement("BUTTON");
+    button.textContent = "+";
+    button.addEventListener("click", function(e){
+      sTable.tabCreate(undefined, undefined, undefined, true);
+    });
+    tabsWrap.appendChild(button);
 
-  if (!sheetObject[0]) {//если sheetObject пуст, следовательно это первый запуск, создаем один таб
-    console.log("ОБЪЕКТА НЕТ!!!!!!");
-    tabCreate(0, "sheet0", true)
-    sheetCreate(0);
+    if (!sTable.sheetObject[0]) {//если sTable.sheetObject пуст, следовательно это первый запуск, создаем один таб
+      sTable.tabCreate(0, "sheet0", true)
+      sTable.sheetCreate(0);
 
-  } else { //если в sheetObject есть данные о листах, строим табы в соответствии с sheetObject
+    } else { //если в sTable.sheetObject есть данные о листах, строим табы в соответствии с sTable.sheetObject
 
-    for (var sheet in sheetObject) {
-      var sheetIndex = parseInt(sheet)
-      var name = sheetObject[sheet].settings.name;
-      var current = sheetObject[sheet].settings.current;
-      tabCreate(sheetIndex, name, current);
+      for (var sheet in sTable.sheetObject) {
+        var sheetIndex = parseInt(sheet)
+        var name = sTable.sheetObject[sheet].settings.name;
+        var current = sTable.sheetObject[sheet].settings.current;
+        sTable.tabCreate(sheetIndex, name, current);
+      }
+
+    };
+
+    sTable.tabSwitching();
+
+  };
+
+
+  sTable.tabCreate = function(counter, name, ifCurrent, isNew) {
+
+    var lastInput = document.querySelector("#tabs-wrap input:last-of-type");
+    if (counter == undefined) {
+      if (lastInput != undefined) {
+        var lastInputIndex = parseInt(lastInput.value);
+        counter = ++lastInputIndex;
+      };
+    }
+    if (name == undefined) {
+      name = "sheet" + counter;
+    }
+
+    var tabsWrap = document.querySelector("#tabs-wrap");
+
+    var input = document.createElement("INPUT");
+    input.id = "tab" + counter;
+    input.type = "radio";
+    input.name = "tab";
+    input.value = counter;
+    if (ifCurrent == true) {//установка активного таба
+      input.checked = true;
+    };
+
+    var label = document.createElement("LABEL");
+    label.setAttribute("for", "tab" + counter);
+    label.textContent = name;
+
+    var deleteTab = document.createElement("DIV");
+    deleteTab.className = "del";
+    deleteTab.textContent = "X";
+
+    tabsWrap.appendChild(input);
+    tabsWrap.appendChild(label);
+    label.appendChild(deleteTab);
+
+    if (isNew == true) {
+      sTable.sheetCreate(counter);
+      document.querySelector("[name='tab'][value='" + counter + "']").checked = true;
+      sTable.fillCells();
     }
 
   };
 
-  tabSwitching();
 
-})();
+  sTable.tabDelete = function(index, elements) {
 
+    //удаляем таб
+    var tabsWrap = document.querySelector("#tabs-wrap");
+    elements.forEach(function(item, i, arr){
+      tabsWrap.removeChild(item);
+    });
 
-function tabCreate(counter, name, ifCurrent, isNew) {
+    //удаляем объект листа и обновляем localStorage
+    sTable.deleteSheet(index);
 
-  var lastInput = document.querySelector("#tabs-wrap input:last-of-type");
-  if (counter == undefined) {
-    if (lastInput != undefined) {
-      var lastInputIndex = parseInt(lastInput.value);
-      counter = ++lastInputIndex;
-    };
-  }
-  if (name == undefined) {
-    name = "sheet" + counter;
-  }
+    //устанавливаем нужный таб и перерисовываем страницу
+    var sheetToRedirect = sTable.setReaddressSheet(index)
 
-  var tabsWrap = document.querySelector("#tabs-wrap");
+    if (sheetToRedirect == undefined) {// если undefined — значит это был единственный лист, и нужно создать новый
 
-  var input = document.createElement("INPUT");
-  input.id = "tab" + counter;
-  input.type = "radio";
-  input.name = "tab";
-  input.value = counter;
-  if (ifCurrent == true) {//установка активного таба
-    input.checked = true;
+      sTable.tabCreate(0, "sheet0", true)
+      sTable.sheetCreate(0);
+      sTable.fillCells();
+
+    } else {
+
+      document.querySelector("#tabs-wrap #tab" + sheetToRedirect).checked = true;
+      sTable.fillCells();
+      sTable.setCurrentSheet(sheetToRedirect);
+
+    }
+
   };
 
-  var label = document.createElement("LABEL");
-  label.setAttribute("for", "tab" + counter);
-  label.textContent = name;
 
-  var deleteTab = document.createElement("DIV");
-  deleteTab.className = "del";
+  sTable.setReaddressSheet = function(index) {
 
-  tabsWrap.appendChild(input);
-  tabsWrap.appendChild(label);
-  label.appendChild(deleteTab);
-
-  if (isNew == true) {
-    sheetCreate(counter);
-    document.querySelector("[name='tab'][value='" + counter + "']").checked = true;
-    fillCells();
-  }
-
-};
-
-
-function tabDelete(index, elements) {
-
-  //удаляем таб
-  var tabsWrap = document.querySelector("#tabs-wrap");
-  elements.forEach(function(item, i, arr){
-    tabsWrap.removeChild(item);
-  });
-
-  //удаляем объект листа и обновляем localStorage
-  deleteSheet(index);
-
-  //устанавливаем нужный таб и перерисовываем страницу
-  var sheetToRedirect = setReaddressSheet(index)
-
-  if (sheetToRedirect == undefined) {// если undefined — значит это был единственный лист, и нужно создать новый
-
-    tabCreate(0, "sheet0", true)
-    sheetCreate(0);
-    fillCells();
-
-  } else {
-
-    document.querySelector("#tabs-wrap #tab" + sheetToRedirect).checked = true;
-    fillCells();
-
-  }
-
-
-};
-
-
-function setReaddressSheet(index) {
-
-  //выясняем на какой таб будет переадресация после удаления
-  var previousElement;//кодовое число, будет использоваться в случае если это первый лист
-  for (var sheet in sheetObject) {
-      if (previousElement != undefined) {//если первый лист, проверяем не единственный ли это лист
-        if (sheet != index) {
-          return sheet;
+    //выясняем на какой таб будет переадресация после удаления
+    var previousElement;//кодовое число, будет использоваться в случае если это первый лист
+    for (var sheet in sTable.sheetObject) {
+        if (previousElement != undefined) {//если первый лист, проверяем не единственный ли это лист
+          if (sheet != index) {
+            return sheet;
+            break;
+          }
+          return previousElement;
           break;
         }
-        return previousElement;
-        break;
+      previousElement = sheet;
+    }
+
+  };
+
+
+  sTable.tabSwitching = function() {
+
+    var tabsWrap = document.querySelector("#tabs-wrap");
+    tabsWrap.addEventListener("click", function(e){
+      if (e.target.nodeName == "LABEL") {
+
+        var clickedTabIndex = e.target.previousElementSibling.value;
+        sTable.setCurrentSheet(clickedTabIndex);
+        sTable.fillCells(clickedTabIndex);
+
       }
-    previousElement = sheet;
-  }
+      console.dir(e.target);
+      if (e.target.className == "del") {
 
-};
+        var tabToDelete = [e.target.parentElement, e.target.parentElement.previousElementSibling]
+        var clickedButtonIndex = parseInt(e.target.parentElement.previousElementSibling.value);
+        sTable.tabDelete(clickedButtonIndex, tabToDelete);
 
-
-function tabSwitching() {
-
-  var tabsWrap = document.querySelector("#tabs-wrap");
-  tabsWrap.addEventListener("click", function(e){
-    if (e.target.nodeName == "LABEL") {
-
-      var clickedTabIndex = e.target.previousElementSibling.value;
-      setCurrentSheet(clickedTabIndex);
-      fillCells(clickedTabIndex);
-
-    }
-    console.dir(e.target);
-    if (e.target.className == "del") {
-
-      var tabToDelete = [e.target.parentElement, e.target.parentElement.previousElementSibling]
-      var clickedButtonIndex = parseInt(e.target.parentElement.previousElementSibling.value);
-      tabDelete(clickedButtonIndex, tabToDelete);
-
-    }
-  });
-};
-
-//заполнение ячеек сохраненными данными
-function fillCells(currentSheet) {
-
-  if (currentSheet == undefined) {
-    var currentSheet = document.querySelector("[name='tab']:checked").value;
-  }
-
-  var cells = document.querySelectorAll("td");
-  cells = Array.prototype.slice.call(cells);
-  for (var cell in cells) {
-    cells[cell].textContent = null;
-  }
-  var table = document.getElementById("super-table");
-
-  for (var row in sheetObject[currentSheet]) {
-    if (row != "settings") {
-      for (var cell in sheetObject[currentSheet][row]) {
-        table.rows[row].cells[cell].textContent = sheetObject[currentSheet][row][cell];
-      }
-    }
-  }
-};
-
-
-tableInit("table-wrap");
-function tableInit(block, width, height, cellWidth, cellHeight){
-
-  var charArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var parentBlock = document.getElementById(block);
-
-  var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
-  var heightOfTable = viewportHeight - parseInt(window.getComputedStyle(parentBlock).getPropertyValue('margin-top'));
-  if (width == undefined)  {parentBlock.style.width  = (viewportWidth + "px");}
-  if (height == undefined) {parentBlock.style.height = (heightOfTable + "px");}
-  if (cellWidth == undefined) {cellWidth = 80;}
-  if (cellHeight == undefined) {cellHeight = 20;}
-
-  var rowsInitialAmount = Math.ceil(viewportHeight / cellHeight);
-  var columnsInitialAmount = Math.ceil(viewportWidth / cellWidth);
-
-  //создание DOM таблицы
-  var creatingTableDom = (function() {
-
-    var tableFragment = document.createDocumentFragment();
-    var table = document.createElement("TABLE");
-    table.id = "super-table";
-    tableFragment.appendChild(table)
-
-    for (var i = 0; i < rowsInitialAmount; i++) {
-      var row = table.insertRow();
-      for (var j = 0; j < columnsInitialAmount; j++) {
-        row.insertCell();
-      }
-    }
-    parentBlock.appendChild(table);
-    fillCells();//Заполняем ячейки
-    //Навешиваем события на клик и блур
-    table.addEventListener("click", function(e){
-      if (e.target.tagName == "TD") {
-
-        var rowIndex = e.target.parentElement.rowIndex;
-        var cellIndex = e.target.cellIndex;
-        var targetCell = table.tBodies[0].rows[rowIndex].cells[cellIndex];
-        var input = document.createElement("INPUT");
-
-        input.value = targetCell.textContent;
-        targetCell.textContent = null;
-        targetCell.appendChild(input);
-        input.focus();
-
-        input.onblur = function(){
-
-          var currentSheet = document.querySelector("[name='tab']:checked").value;
-
-          if (!sheetObject[currentSheet].hasOwnProperty(rowIndex)) {
-            sheetObject[currentSheet][rowIndex] = {};
-          }
-
-          sheetObject[currentSheet][rowIndex][cellIndex] = input.value;
-
-          updateSheet();
-
-          input.parentElement.textContent = input.value;
-          input.remove();
-          input = null;
-
-        };
       }
     });
 
-  })();
-
-};
+  };
 
 
-function setCurrentSheet(index) {
+  sTable.fillCells = function(currentSheet) {
 
-  for (var sheet in sheetObject) {
-    sheetObject[sheet].settings.current = false
-  }
+    if (currentSheet == undefined) {
+      var currentSheet = document.querySelector("[name='tab']:checked").value;
+    }
 
-  sheetObject[index].settings.current = true;
+    var cells = document.querySelectorAll("td");
+    cells = Array.prototype.slice.call(cells);
+    for (var cell in cells) {
+      cells[cell].textContent = null;
+    }
+    var table = document.getElementById("super-table");
 
-  localStorage.sheets = JSON.stringify(sheetObject);
-
-};
-
-
-function deleteSheet(index) {
-
-  delete sheetObject[index];
-  updateSheet();
-
-};
-
-
-function sheetCreate(index) {
-
-  sheetObject[index] = 
-  {
-    "settings" : 
-    {
-      "name" : "sheet" + index,
-      "current" : true
+    for (var row in sTable.sheetObject[currentSheet]) {
+      if (row != "settings") {
+        for (var cell in sTable.sheetObject[currentSheet][row]) {
+          table.rows[row].cells[cell].textContent = sTable.sheetObject[currentSheet][row][cell];
+        }
+      }
     }
   };
-  setCurrentSheet(index);
-  localStorage.sheets = JSON.stringify(sheetObject);
-
-};
 
 
-function updateSheet() {
+  sTable.setCurrentSheet = function(index) {
 
-  localStorage.sheets = JSON.stringify(sheetObject);
+    for (var sheet in sTable.sheetObject) {
+      sTable.sheetObject[sheet].settings.current = false
+    }
 
-};
+    sTable.sheetObject[index].settings.current = true;
+
+    localStorage.sheets = JSON.stringify(sTable.sheetObject);
+
+  };
+
+
+  sTable.deleteSheet = function(index) {
+
+    delete sTable.sheetObject[index];
+    sTable.updateSheet();
+
+  };
+
+
+  sTable.sheetCreate = function(index) {
+
+    sTable.sheetObject[index] = 
+    {
+      "settings" : 
+      {
+        "name" : "sheet" + index,
+        "current" : true
+      }
+    };
+    sTable.setCurrentSheet(index);
+    localStorage.sheets = JSON.stringify(sTable.sheetObject);
+
+  };
+
+
+  sTable.updateSheet = function() {
+
+    localStorage.sheets = JSON.stringify(sTable.sheetObject);
+
+  };
+
+document.addEventListener("DOMContentLoaded", sTable.init("table-wrap"));
